0ed82dd10f3dad61f0b2b3559948fc70070032ab
haneenabdullah <haneen073s@gmail.com>
2020-04-13 05:03:37 +0300

    SLP view progress

--- Login/Controllers/ProgressViewController.swift
+++ Login/Controllers/ProgressViewController.swift
@@ -7,63 +7,205 @@
 //
 
 import UIKit
+import Foundation
 import Firebase
 import MessageUI
 
-class ProgressViewController: UIViewController, MFMailComposeViewControllerDelegate {
-    @IBOutlet var sendProgress: UIButton!
-     let db = Firestore.firestore()
-    var pEmail = String()
+//class ProgressViewController: UIViewController, MFMailComposeViewControllerDelegate{
+class ProgressViewController: UIViewController, MFMailComposeViewControllerDelegate, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet var sendEmail: UIBarButtonItem!
-    
-    @IBAction func sendEmailPressed(_ sender: UIButton) {
-    
-    if MFMailComposeViewController.canSendMail() {
-                    print("email")
-                let mail = MFMailComposeViewController()
-                mail.mailComposeDelegate = self
-                db.collection("patients").whereField("uid", isEqualTo:Auth.auth().currentUser!.uid).getDocuments { (snapshot, error) in
-                    if let error = error {
-                        print(error.localizedDescription)
-                    } else {
-                        if let snapshot = snapshot {
-                            
-                            for document in snapshot.documents {
-                                let data = document.data()
-                                self.pEmail = data["Email"] as! String
-                                print (self.pEmail)
-                                //print("I send email")
-                            }
-                        }
-                    }
-                    //mail.setToRecipients(["horalfaisal2016@gmail.com"])
-                    //mail.setToRecipients([self.pEmail])
-                    //mail.setToRecipients(["horalfaisal2016@gmail.com","horalfaisal2016@gmail.com"])
-                }
-                 //message.append(Auth.auth().currentUser!.email!)
-        mail.setToRecipients([self.pEmail])
-                    mail.setSubject("تقدم حالة المريض")
-                    mail.setMessageBody("هنا سيكون تقدم حالة المريض", isHTML: false)
-                    self.present(mail, animated: true)
-                    
-                } else {
-                    // show failure alert
-                
-                            print("Cannot send email")
-        // create the alert
-        let alert = UIAlertController(title:"" , message: "لم يتم ربط هذا الجهاز بحساب البريد الإلكتروني. يرجى تسجيل الدخول إلى بريدك الإلكتروني في برنامج Mail,Gmail,...", preferredStyle: UIAlertController.Style.alert)
-        
-        // add an action (button)
-        
-        alert.addAction(UIAlertAction(title: "حسنا", style: UIAlertAction.Style.default, handler: nil))
-        
-        // show the alert
-        self.present(alert, animated: true, completion: nil)
 
+    
+
+
+
+//    class ProgressViewController: UITableViewController, MFMailComposeViewControllerDelegate {
+
+        @IBOutlet var tableView: UITableView!
+        
+    
+    @IBOutlet weak var sendButton: UIBarButtonItem!
+    
+    var progressArray = [Progress]()
+    var db: Firestore!
+    
+    
+    //    var progress : Progress
+    //    var answer = ""
+    //    var result = ""
+    //    var date = ""
+    
+    var pEmail = String()
+        
+    
+    
+     //Tableview setup
+        func numberOfSections(in tableView: UITableView) -> Int {
+    
+            return 1
+        }
+    //
+    //            func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //
+    //                        print("Tableview setup \(progressArray.count)")
+    //                return progressArray.count
+    //            }
+    //
+    //            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //                let cell = tableView.dequeueReusableCell(withIdentifier: "progressCell", for: indexPath) as! ProgressCell
+    //
+    //          cell.configurateTheCell(progressArray[indexPath.row])
+    //                print("Array is populated \(progressArray)")
+    //        //        cell.accessoryType.
+    //
+    //
+    //                return cell
+    //            }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+                              print("Tableview setup \(progressArray.count)")
+                          return progressArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+                  let cell = tableView.dequeueReusableCell(withIdentifier: "progressCell", for: indexPath) as! ProgressCell
+           
+                     cell.configurateCell(progressArray[indexPath.row])
+                           print("Array is populated \(progressArray)")
+                   //        cell.accessoryType.
+           
+           
+                           return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let value = UIInterfaceOrientation.portrait.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+                       tableView.dataSource = self
+                       tableView.delegate = self
+        
+        db = Firestore.firestore()
+        loadData()
+    }
+    
+    func loadData() {
+        
+        
+        var progresses = [String()]
+        
+        let db = Firestore.firestore()
+        
+        let uID = "SAYOVleHD3XWWMphbXTMtsPQaYg2"
+        
+        db.collection("patients").whereField("uid", isEqualTo: uID ).getDocuments { (snapshot, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                if let snapshot = snapshot {
+                    
+                    for document in snapshot.documents {
+                        
+                        let data = document.data()
+                        
+                        //set one progress
+                        progresses = data["progress"] as! [String]
+                        
+                        
+                        //                                                var progress =  setProgress(singleProgress: progresses)
+                        ////-----------
+                        for singleProgress in progresses {
+                            let fullTrail = singleProgress
+                            let p : [Progress] = self.setProgress(singleProgress : fullTrail)
+                            
+                            self.progressArray.append(contentsOf: p)
+                        }
+                        //-------or
+                        //                        for sProgress in tProgress
+                        //self.progressArray.append(sProgress)
+                        //}
+                        
+                        //
+                        //                        }
+                        
+                        //----for test
+                        //                        var fullTrail = progresses[0]
+                        //
+                        //                        var p : [Progress] = self.setProgress(singleProgress : fullTrail)
+                        //
+                        //                        self.progressArray.append(contentsOf: p)
+                        
+                    }
+                    
+                    
+                    
+                    self.tableView.reloadData()
+                    
+                    
                 }
             }
+        }
+        
+        
+        
+        
+    }
+
+
+    
+    @IBOutlet weak var sendEmail: UIBarButtonItem!
+    
+    
+    
+    @IBAction func sendEmailPressed(_ sender: Any) {
+        
+        if MFMailComposeViewController.canSendMail() {
+            print("email")
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            db.collection("patients").whereField("uid", isEqualTo:Auth.auth().currentUser!.uid).getDocuments { (snapshot, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    if let snapshot = snapshot {
+                        
+                        for document in snapshot.documents {
+                            let data = document.data()
+                            self.pEmail = data["Email"] as! String
+                            print (self.pEmail)
+                            //print("I send email")
+                        }
+                    }
+                }
+                //mail.setToRecipients(["horalfaisal2016@gmail.com"])
+                //mail.setToRecipients([self.pEmail])
+                //mail.setToRecipients(["horalfaisal2016@gmail.com","horalfaisal2016@gmail.com"])
+            }
+            //message.append(Auth.auth().currentUser!.email!)
+            mail.setToRecipients([self.pEmail])
+            mail.setSubject("تقدم حالة المريض")
+            mail.setMessageBody("هنا سيكون تقدم حالة المريض", isHTML: false)
+            self.present(mail, animated: true)
             
+        } else {
+            // show failure alert
+            
+            print("Cannot send email")
+            // create the alert
+            let alert = UIAlertController(title:"" , message: "لم يتم ربط هذا الجهاز بحساب البريد الإلكتروني. يرجى تسجيل الدخول إلى بريدك الإلكتروني في برنامج Mail,Gmail,...", preferredStyle: UIAlertController.Style.alert)
+            
+            // add an action (button)
+            
+            alert.addAction(UIAlertAction(title: "حسنا", style: UIAlertAction.Style.default, handler: nil))
+            
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+    }
+    
     // func configureMailComposer() -> MFMailComposeViewController{
     //             let mailComposeVC = MFMailComposeViewController()
     //             mailComposeVC.mailComposeDelegate = self
@@ -86,34 +228,95 @@ class ProgressViewController: UIViewController, MFMailComposeViewControllerDeleg
     //
     //
     
-       func mailComposeController(_ controller:MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+    func mailComposeController(_ controller:MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         switch result.rawValue {
-                    case MFMailComposeResult.cancelled.rawValue:
-                        print("Cancelled")
-                    case MFMailComposeResult.saved.rawValue:
-                        print("Saved")
-                    case MFMailComposeResult.sent.rawValue:
-                        print("Sent")
-                    case MFMailComposeResult.failed.rawValue:
-                        print("Error: \(String(describing: error?.localizedDescription))")
-                    default:
-                        break
-                    }
-       controller.dismiss(animated: true)
-       }
-
-      
-        
-        
-        
-        
-        
-      
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        case MFMailComposeResult.cancelled.rawValue:
+            print("Cancelled")
+        case MFMailComposeResult.saved.rawValue:
+            print("Saved")
+        case MFMailComposeResult.sent.rawValue:
+            print("Sent")
+        case MFMailComposeResult.failed.rawValue:
+            print("Error: \(String(describing: error?.localizedDescription))")
+        default:
+            break
+        }
+        controller.dismiss(animated: true)
     }
     
-
+    
+    func setProgress(singleProgress : String) -> [Progress] {
+        var tProgress = [Progress]()
+        var progress : Progress
+        let trialProgress = singleProgress.components(separatedBy: ",")
+        
+        let trialAnswers = trialProgress[0]
+        let trialResults = trialProgress[1]
+        let trialDate = trialProgress[2]
+        //                                    for trialData in trialProgress{
+        //                                        print("\(trialData) ")
+        //                                    }
+        //                                           print("-------")
+        let answers = trialAnswers.components(separatedBy: "-")
+        
+        //                                                                 for trialData in answers{
+        //                                                                            print("\(trialData) ")
+        //                                                                        }
+        print("-------")
+        let results = trialResults.components(separatedBy: "-")
+        
+        //                                                  for trialData in results{
+        //                                                             print("\(trialData) ")
+        //                                                         }
+        print("----=========---")
+        
+        
+        var index = 0
+        while index < answers.count {
+            //                                        self.answer = answers[index]
+            //                                        self.result = results[index]
+            //                                        self.date = trialDate
+            
+            progress = Progress ( answer: answers[index] , result: results[index] , date: trialDate )
+            
+            print("\(answers[index])    \(results[index])    \(trialDate)")
+            index += 1
+            tProgress.append( progress  )
+            
+            print("        -------")
+        }
+        
+        print("----=========---")
+        //------------
+        
+        //            var progress = Progress (answers, answer,results result,date date)
+        return tProgress
+    }
+    
+    
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+           super.didReceiveMemoryWarning()
+           // Dispose of any resources that can be recreated.
+       }
+    
+    
+    // ------------for disable rotate > portrait view only
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    //       override open var shouldAutorotate: Bool {
+    //           return false
+    //       }
+    
 }
---
+++ Login/Model/Progress.swift
@@ -0,0 +1,42 @@
+//
+//  Progress.swift
+//  Talaqah
+//
+//  Created by Haneen Abdullah on 26/03/2020.
+//  Copyright © 2020 Gary Tokman. All rights reserved.
+//
+
+import Foundation
+
+struct Progress {
+    
+    
+    var answer:String
+    var result:String
+    var date:String
+    
+    
+    init(answer: String, result: String, date: String) {
+        self.answer = answer
+        self.result = result
+        self.date = date
+    }
+    
+//    var answers:[String]
+//    var results:[String]
+//    var date:String
+//    var progress:String
+    
+    
+//    func getProgress () -> String {
+//
+//
+//return progress
+//    }
+    
+//    func Progress (answers : [String], results :[String], date : String) {
+//      answers=answers
+//        results=results
+//        date=date
+//    }
+}
